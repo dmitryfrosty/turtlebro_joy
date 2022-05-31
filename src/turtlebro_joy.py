3be047b41eea0d0e864486b5fc02ab0ff81cf52d
dmitryfrosty <diman.morozov16@yandex.ru>
2022-05-31 12:51:57 +0700

    optimization

--- src/turtlebro_joy.py
+++ src/turtlebro_joy.py
@@ -4,24 +4,18 @@ import rospy
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 
-
 rospy.init_node("turtlebro_joy")
 pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
 
 def callback(msg):
-    linear = cd(msg.axes[1], -1.0, 1.0, -0.2, 0.2)
-    angular = cd(msg.axes[0], -1.0, 1.0, -1.5, 1.5)
-    vel(linear, angular)
+    pub_vel = Twist()
+    pub_vel.linear.x = cd(msg.axes[1], -1.0, 1.0, -0.2, 0.2)
+    pub_vel.angular.z = cd(msg.axes[0], -1.0, 1.0, -1.5, 1.5)
+    pub.publish(pub_vel)
 
 def cd(old, old_min, old_max, new_min, new_max):
     new = (((old - old_min) * (new_max - new_min)) / (old_max - old_min)) + new_min
     return(new)
-
-def vel(velx, velz):
-    pub_vel = Twist()
-    pub_vel.linear.x = velx
-    pub_vel.angular.z = velz
-    pub.publish(pub_vel)
 
 rospy.Subscriber("/joy_orig", Joy, callback)
 
